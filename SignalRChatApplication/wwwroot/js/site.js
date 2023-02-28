e61f0e6a813ec1f2ba12e677a290962d2f2f226e
didem-damka <didem.damka@gmail.com>
2023-03-01 00:06:18 +0300

    template added

--- SignalRChatApplication/wwwroot/js/site.js
+++ SignalRChatApplication/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+'<li class="clearfix">' +
+    '<div class="message-data text-right">' +
+    '<span class="message-data-time">'+user+'</span>' +
+    '</div>' +
+    '<div class="message other-message float-right"> '+msg+ '</div>' +
+    '</li>';
