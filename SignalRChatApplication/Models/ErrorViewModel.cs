b7befb0b38e5eb53978a711012cf71b57fc4355a
didem-damka <didem.damka@gmail.com>
2023-02-28 18:18:49 +0300

    init

---
+++ SignalRChatApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRChatApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
